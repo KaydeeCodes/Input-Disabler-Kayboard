73b02508d8fa4d3aab29f76003b4b8bd0202447e
Kaydee Fisher <kaydeecodes@gmail.com>
2025-04-28 09:49:55 +0100

    Rebuild & Run - Testing

--- InputDisabler_K/obj/Debug/net9.0/InputDisabler_K.AssemblyInfo.cs
+++ InputDisabler_K/obj/Debug/net9.0/InputDisabler_K.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InputDisabler_K")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+818b94976496b0158e477a9127a48d472393f3b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("InputDisabler_K")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InputDisabler_K")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
